a0ab6ed33f983f9dac8d179353578d18bb4316ae
KiruthickK <kiruthick101@gmail.com>
2023-04-07 09:49:37 +0530

    Intermediate changes

---
+++ DrawShapes.py
@@ -0,0 +1,17 @@
+# Python program to demonstrate
+# circle drawing  
+import turtle
+t = turtle.Turtle()
+# Initializing the turtle
+def DrawCircle():
+    r = 50
+    t.circle(r)
+def TangentCircle():
+    r = 10
+    # number of circles
+    n = 10
+    # loop for printing tangent circles
+    for i in range(1, n + 1, 1):
+        t.circle(r * i)
+def ClearDrawings():
+    t.clear()
--- Main.py
+++ Main.py
@@ -1,2 +1,18 @@
 import VoiceRecognition as VR
+import DrawShapes as DS
+while(True):
+    text = VR.SpeechToText()
+    if(text is None):
+        continue
+    elif(text == "done"):
+        break
+    elif(text == "round"):
+        print("Here")
+        DS.DrawCircle()
+    elif(text == "circle"):
+        DS.TangentCircle()
+    elif(text == "clear"):
+        DS.ClearDrawings()
+    else:
+        print("Hai")
 
